58f46c32336fd3fa1980a16cb48339def79da615
Edward Todd <edwardtdd@gmail.com>
2022-05-02 13:55:16 +0100

    First commit

---
+++ src/main/java/org/example/_2_basics/Month.java
@@ -0,0 +1,5 @@
+package org.example._2_basics;
+
+public enum Month {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+}
---
+++ src/main/java/org/example/_2_basics/PizzaStatus.java
@@ -0,0 +1,5 @@
+package org.example._2_basics;
+
+public enum PizzaStatus {
+    ORDERED,READY,DELIVERED
+}
